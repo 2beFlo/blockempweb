52afbf86eefa555ff2d673cbcd313adb105d127c
2beFlo <info@florianlillpopp.de>
2016-05-08 03:01:46 +0200

    geändert:       impressum.php

--- v5/impressum.php
+++ v5/impressum.php
@@ -12,14 +12,8 @@
     <title>Block Empirium - Der Minecraft-Server ab 18!</title>
     
 	 <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
-
-    <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
     <link href="css/agency.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
@@ -36,10 +30,8 @@
 </head>
 
 <body id="page-top" class="index">
-    <!-- Navigation -->
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header page-scroll">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
@@ -50,7 +42,6 @@
                 <a class="navbar-brand page-scroll" href="#page-top">Block Empirium</a>
             </div>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     <li class="hidden">
@@ -80,12 +71,9 @@
 		    </li>
                 </ul>
             </div>
-            <!-- /.navbar-collapse -->
         </div>
-        <!-- /.container-fluid -->
     </nav>
 
-<!-- Header -->
     <header>
         <div class="container">
             <div class="intro-text">
@@ -93,22 +81,17 @@
             </div>
         </div>
     </header>
-
-
-   
-	 
-	 <!-- Detail Section -->
+ 
     <section id="detail">
-			<div class="container"  id="services">
+			<div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Impressum</h2>
                     <h6 id="impressumgem5tmgund55rstv">Impressum gemäß § 5 TMG und § 55 RStV</h6>
 
 <p><em>inhaltlich verantwortlich gemäß § 55 Abs. 2 RStV:</em></p>
 
 <p>Florian Lillpopp <br>
-florian@chartstoffarm.de</p>
+info@blockempirium.de</p>
 
 <p><strong>Haftungshinweis:</strong> Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.</p>
                 </div>
@@ -139,22 +122,14 @@ florian@chartstoffarm.de</p>
             </div>
         </div>
     </footer>
-    <!-- jQuery -->
+
     <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-
-    <!-- Plugin JavaScript -->
     <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
     <script src="js/classie.js"></script>
     <script src="js/cbpAnimatedHeader.js"></script>
-
-    <!-- Contact Form JavaScript -->
     <script src="js/jqBootstrapValidation.js"></script>
     <script src="js/contact_me.js"></script>
-
-    <!-- Custom Theme JavaScript -->
     <script src="js/agency.js"></script>
 
 </body>
