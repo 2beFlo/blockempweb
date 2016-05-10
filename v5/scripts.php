3d3a92ad53de8e8af37afbd575be497b84c994ba
2beFlo <info@florianlillpopp.de>
2016-05-10 02:43:15 +0200

    typo

---
+++ v5/scripts.php
@@ -0,0 +1,8 @@
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+    <script src="js/agency.js"></script>
