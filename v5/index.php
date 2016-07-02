96b55b8d9a0e5f4e9376a4808b8772a679f05e97
2beFlo <unconfigured@null.spigotmc.org>
2016-07-02 18:13:09 +0200

    div css stuff

--- v5/index.php
+++ v5/index.php
@@ -242,7 +242,7 @@
                         <i class="fa fa-cog fa-stack-1x fa-inverse"></i>
                     </span>
                     <h4 class="service-heading">Innovative Plugins</h4>
-                    <p class="text-muted">Mit hausgemachten Plugins gelingt es uns in unserer Welt für eine besondere Atmosphäre zu sorgen. Vom Flugmeister über unsere Vending Machines bis hin zum innovativen Armorstand Powerhouse "Art Deko gibt es nichts, was wir nicht haben. Mit kleinen subtilen Verbesserungen am Gameplay erweitern wir unser Survival und Projekt Konzept, immer mit dem Ziel unseren Spielern das optimale Umfeld für die Umsetzung ihrer Ideen zu ermöglichen.</p>
+                    <p class="text-queit">Mit hausgemachten Plugins gelingt es uns in unserer Welt für eine besondere Atmosphäre zu sorgen. Vom Flugmeister über unsere Vending Machines bis hin zum innovativen Armorstand Powerhouse "Art Deko gibt es nichts, was wir nicht haben. Mit kleinen subtilen Verbesserungen am Gameplay erweitern wir unser Survival und Projekt Konzept, immer mit dem Ziel unseren Spielern das optimale Umfeld für die Umsetzung ihrer Ideen zu ermöglichen.</p>
                 </div>
                 <div class="col-md-4">
                     <span class="fa-stack fa-4x">
@@ -250,7 +250,7 @@
                         <i class="fa fa-users fa-stack-1x fa-inverse"></i>
                     </span>
                     <h4 class="service-heading">Reife Comunity</h4>
-                    <p class="text-muted">Blockempirium richtet sich für Minecraftverhältnisse vor allem an ältere Spieler. Unser formuliertes Einstiegsalter „ab 18 Jahren“ dient uns dabei seit jeher als Orientierung. Selbstverständlich kann eine Altersgrenze nicht alles sein. Daher haben wir uns bei der Aufnahme neuer Mitglieder für ein Bewerbungssystem entschieden, was es uns ermöglicht eine harmonische Community zu formen die mit einem Minimum an Regeln vor allem auf Gemeinsamkeiten aufgebaut ist. Jedes Mitglied auf Blockempirium ist dabei willkommen, mit seinen oder ihren individuellen Fähigkeiten zur weiteren Verbesserung des Serverprojekts beizutragen. Ob im Team oder nicht, gute Ideen und Vorschläge unserer Spieler finden bei uns immer ein offenes Ohr.</p>
+                    <p class="text-queit">Blockempirium richtet sich für Minecraftverhältnisse vor allem an ältere Spieler. Unser formuliertes Einstiegsalter „ab 18 Jahren“ dient uns dabei seit jeher als Orientierung. Selbstverständlich kann eine Altersgrenze nicht alles sein. Daher haben wir uns bei der Aufnahme neuer Mitglieder für ein Bewerbungssystem entschieden, was es uns ermöglicht eine harmonische Community zu formen die mit einem Minimum an Regeln vor allem auf Gemeinsamkeiten aufgebaut ist. Jedes Mitglied auf Blockempirium ist dabei willkommen, mit seinen oder ihren individuellen Fähigkeiten zur weiteren Verbesserung des Serverprojekts beizutragen. Ob im Team oder nicht, gute Ideen und Vorschläge unserer Spieler finden bei uns immer ein offenes Ohr.</p>
                 </div>
                 <div class="col-md-4">
                     <span class="fa-stack fa-4x">
@@ -258,7 +258,7 @@
                         <i class="fa fa-university fa-stack-1x fa-inverse"></i>
                     </span>
                     <h4 class="service-heading">Gehobener Baustil</h4>
-                    <p class="text-muted">Ein Markenzeichen unseres Servers ist sein stetes Streben nach guter Baukunst. Hier findet ihr nicht nur qualitativ Hochwertig gebaute Projekte aus der Vergangenheit des Servers, sondern auch Mitspieler, welche den Bau-Aspekt des Spiels lieben. Seit vielen Jahren ist Blockempirium dabei eine Ort der Innovation und des gemeinsamen Lernens im Punkto Bauen. Mit unserem umfangreichen Erfahrungsschatz in den verschiedensten Baustilen, konnten wir bisher noch jedem Neuankömmling den einen oder anderen Kniff beibringen.</p>
+                    <p class="text-queit">Ein Markenzeichen unseres Servers ist sein stetes Streben nach guter Baukunst. Hier findet ihr nicht nur qualitativ Hochwertig gebaute Projekte aus der Vergangenheit des Servers, sondern auch Mitspieler, welche den Bau-Aspekt des Spiels lieben. Seit vielen Jahren ist Blockempirium dabei eine Ort der Innovation und des gemeinsamen Lernens im Punkto Bauen. Mit unserem umfangreichen Erfahrungsschatz in den verschiedensten Baustilen, konnten wir bisher noch jedem Neuankömmling den einen oder anderen Kniff beibringen.</p>
                 </div>
             </div>
         </div>
