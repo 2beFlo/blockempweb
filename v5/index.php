6594895f97a604deec82c9631dfbe480e7eb96ac
2beFlo <unconfigured@null.spigotmc.org>
2016-07-02 22:36:16 +0200

    minor

--- v5/index.php
+++ v5/index.php
@@ -83,7 +83,7 @@
                     <p class="text-muted"><a href="http://mc.blockempirium.de:8123" target="_blank">mc.blockempirium.de:8123</a></p>
                 </div>
                 <div class"spaceding">
-                <h3 class="section-heading">&npsb;<br/><br/>Hier steht eine Überschrift</h3>
+                <h3 class="section-heading">&nbsp;<br/><br/>Hier steht eine Überschrift</h3>
                 <p class="text-silent">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
 
 Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.   
