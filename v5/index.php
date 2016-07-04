afd75d1e1afcd685189ed8135b14d0a6dafee900
2beFlo <unconfigured@null.spigotmc.org>
2016-07-04 21:16:19 +0200

    Galerie

--- v5/index.php
+++ v5/index.php
@@ -57,7 +57,23 @@
 				<ol class="carousel-indicators hidden-xs">
 					<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
 					<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-					<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="4"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="5"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="6"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="7"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="8"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="9"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="10"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="12"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="13"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="14"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="15"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="16"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="17"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="18"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="19"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="20"></li>		
 				</ol>
 
 	<!-- Wrapper for slides -->
